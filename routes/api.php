c04b1871135216493b05e644b103fd2c13781618
Natalie Santiago <nataliesantiago@qdata.io>
2019-06-18 11:57:34 -0500

    allow-origin

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('user', 'UserController');
+Route::group(['middleware' => 'cors'], function() {
+    Route::resource('user', 'UserController');
+});
